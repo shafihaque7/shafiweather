718dd107c45577dba8b984b14ad60ef4774e7f9a
Shafi Haque <md_haque@apple.com>
2021-06-13 23:47:50 -0700

    Initial Commit

---
+++ shafiweather/shafiweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  shafiweatherApp.swift
+//  shafiweather
+//
+//  Created by Shafi Haque on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct shafiweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
